bd3e81ce5d87ab00b381f4a09a7172416bb40bb7
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2022-04-02 14:07:10 +0200

    add empty history message

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -27,7 +27,7 @@ const History = () => {
 
   return (
     <>
-      {history ? (
+      {history.length > 0 ? (
         <section className='history__container'>{historyElements}</section>
       ) : (
         <p style={{ fontSize: '1.5rem', marginTop: '3rem' }}>
